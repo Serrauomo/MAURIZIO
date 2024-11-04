46072bc7c60abf3d56e9dee496396c4df644c043
Serrauomo <damianoa928@gmail.com>
2024-11-04 15:18:54 +0100

    Initial Commit

---
+++ MAURIZIO/MAURIZIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAURIZIOApp.swift
+//  MAURIZIO
+//
+//  Created by Davide Serrano on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MAURIZIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
